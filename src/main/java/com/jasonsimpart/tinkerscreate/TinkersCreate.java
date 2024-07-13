bb12ffd27b5556445ede278206b5bf820a2ffe45
CPearl0 <1269596929@qq.com>
2024-07-13 19:23:57 +0800

    Initial commit

---
+++ src/main/java/com/jasonsimpart/tinkerscreate/TinkersCreate.java
@@ -0,0 +1,12 @@
+package com.jasonsimpart.tinkerscreate;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(TinkersCreate.MODID)
+public class TinkersCreate
+{
+    public static final String MODID = "tinkerscreate";
+    private static final Logger LOGGER = LogUtils.getLogger();
+}
