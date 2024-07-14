377d6509987b02e8e1bebc68061f4634878e3fcb
CPearl0 <1269596929@qq.com>
2024-07-14 18:58:28 +0800

    new modifier

--- src/main/java/com/jasonsimpart/tinkerscreate/TinkersCreate.java
+++ src/main/java/com/jasonsimpart/tinkerscreate/TinkersCreate.java
@@ -3,6 +3,7 @@ package com.jasonsimpart.tinkerscreate;
 import com.jasonsimpart.tinkerscreate.register.TinkersCreateBlocks;
 import com.jasonsimpart.tinkerscreate.register.TinkersCreateItems;
 import com.jasonsimpart.tinkerscreate.register.TinkersCreateMaterials;
+import com.jasonsimpart.tinkerscreate.register.TinkersCreateModifiers;
 import com.mojang.logging.LogUtils;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.MinecraftForge;
@@ -25,7 +26,7 @@ public class TinkersCreate {
         MinecraftForge.EVENT_BUS.register(this);
         TinkersCreateItems.ITEMS.register(bus);
         TinkersCreateBlocks.BLOCKS.register(bus);
-
+        TinkersCreateModifiers.MODIFIERS.register(bus);
     }
 
     @SubscribeEvent
--- src/main/java/com/jasonsimpart/tinkerscreate/modifiers/SolidModifier.java
+++ src/main/java/com/jasonsimpart/tinkerscreate/modifiers/SolidModifier.java
@@ -1,12 +1,32 @@
 package com.jasonsimpart.tinkerscreate.modifiers;
 
 import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.tools.context.ToolRebuildContext;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.build.ToolStatsModifierHook;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.nbt.IToolContext;
 import slimeknights.tconstruct.library.tools.stat.ModifierStatsBuilder;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
-public class SolidModifier extends Modifier {
-    public void addToolStats(ToolRebuildContext context, int level, ModifierStatsBuilder builder) {
-    ToolStats.DURABILITY.multiply(builder, 1 + 0.1 * level);
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class SolidModifier extends Modifier implements ToolStatsModifierHook {
+    @Override
+    protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
+        super.registerHooks(hookBuilder);
+        hookBuilder.addHook(this, ModifierHooks.TOOL_STATS);
+    }
+
+    @Override
+    public int getPriority() {
+        return 50;
+    }
+
+    @Override
+    public void addToolStats(IToolContext iToolContext, ModifierEntry modifierEntry, ModifierStatsBuilder modifierStatsBuilder) {
+        float level = modifierEntry.getEffectiveLevel();
+        ToolStats.DURABILITY.multiply(modifierStatsBuilder, 1 + 0.1 * level);
     }
 }
