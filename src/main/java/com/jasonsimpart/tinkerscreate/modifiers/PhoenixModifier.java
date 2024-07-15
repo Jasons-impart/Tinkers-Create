1c9b41e7a86ea81564bd6e068b1c794deb3a11b2
JasonQ1123 <nmuqindexiang@163.com>
2024-07-15 18:27:41 +0800

    增加phoenix的TOOLTYPE显示

--- src/main/java/com/jasonsimpart/tinkerscreate/modifiers/PhoenixModifier.java
+++ src/main/java/com/jasonsimpart/tinkerscreate/modifiers/PhoenixModifier.java
@@ -16,6 +16,8 @@ import slimeknights.tconstruct.library.modifiers.hook.display.TooltipModifierHoo
 import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import slimeknights.tconstruct.library.tools.stat.ToolStats;
+import slimeknights.tconstruct.tools.stats.ToolType;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
@@ -39,16 +41,29 @@ public class PhoenixModifier extends NoLevelsModifier implements LivingDeathModi
             return true;
         if (tool.getCurrentDurability() < 1000)
             return false;
-
         tool.setDamage(tool.getDamage() + 1000);
+        entity.playSound();
         entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 400, 1));
         entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 100, 1));
         entity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 400));
         return true;
     }
-
+    // 声明TOOLTYPE是ARMOR
+    public ToolType[] TYPES = new ToolType[]{ToolType.ARMOR};
     @Override
-    public void addTooltip(IToolStackView iToolStackView, ModifierEntry modifierEntry, @Nullable Player player, List<Component> list, TooltipKey tooltipKey, TooltipFlag tooltipFlag) {
-
+    public void addTooltip(IToolStackView tool, ModifierEntry modifier, @Nullable Player player, List<Component> tooltip, TooltipKey key, TooltipFlag tooltipFlag) {
+        // 传参
+        ToolType type = ToolType.from(tool.getItem(), TYPES);
+        // 判断是否存在对应type
+        if (type != null) {
+            int level = modifier.getLevel();
+            // 计算Phoenix能够生效的次数(向下取整)
+            double bonus =  Math.floor((double) tool.getCurrentDurability() /1000);
+            // 判断是否bonus大于等于1，若存在则显示
+            if (bonus >= 1.0F) {
+                // 添加到hook里
+                TooltipModifierHook.addFlatBoost(this, TooltipModifierHook.statName(this, ToolStats.ARMOR), bonus, tooltip);
+            }
+        }
     }
 }
