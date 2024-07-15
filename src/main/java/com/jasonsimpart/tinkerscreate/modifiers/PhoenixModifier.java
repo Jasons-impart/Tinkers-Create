d69e1b984ace8759f5331b0f98b59d756894697d
JasonQ1123 <nmuqindexiang@163.com>
2024-07-15 15:23:20 +0800

    创建phoenixModifier

--- src/main/java/com/jasonsimpart/tinkerscreate/modifiers/PhoenixModifier.java
+++ src/main/java/com/jasonsimpart/tinkerscreate/modifiers/PhoenixModifier.java
@@ -1,6 +1,13 @@
 package com.jasonsimpart.tinkerscreate.modifiers;
 
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EquipmentSlot;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.hook.armor.ModifyDamageModifierHook;
 import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
+import slimeknights.tconstruct.library.tools.context.EquipmentContext;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
 public class PhoenixModifier extends NoLevelsModifier {
+
 }
