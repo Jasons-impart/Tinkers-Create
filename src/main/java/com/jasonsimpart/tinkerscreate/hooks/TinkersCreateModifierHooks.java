1c2948ac837158296dff23dfd773369799b7536b
CPearl0 <1269596929@qq.com>
2024-07-17 12:17:51 +0800

    Fix ARROW_DAMAGE

--- src/main/java/com/jasonsimpart/tinkerscreate/hooks/TinkersCreateModifierHooks.java
+++ src/main/java/com/jasonsimpart/tinkerscreate/hooks/TinkersCreateModifierHooks.java
@@ -1,12 +1,8 @@
 package com.jasonsimpart.tinkerscreate.hooks;
 
 import com.jasonsimpart.tinkerscreate.Utils;
-import net.minecraft.resources.ResourceLocation;
 import slimeknights.mantle.data.registry.IdAwareComponentRegistry;
-import slimeknights.tconstruct.library.modifiers.hook.behavior.ToolDamageModifierHook;
 import slimeknights.tconstruct.library.module.ModuleHook;
-
-import java.util.function.Function;
 
 public class TinkersCreateModifierHooks {
     public static final IdAwareComponentRegistry<ModuleHook<?>> LOADER = new IdAwareComponentRegistry<>("Unknown Modifier Hook");
@@ -16,5 +12,5 @@ public class TinkersCreateModifierHooks {
     public static final ModuleHook<TargetDeathModifierHook> TARGET_DEATH =
             LOADER.register(new ModuleHook<>(Utils.getResource("target_death"), TargetDeathModifierHook.class, TargetDeathModifierHook.AllMerger::new, (tool, modifier, targetEntity) -> {}));
     public static final ModuleHook<ArrowDamageModifierHook> ARROW_DAMAGE =
-            LOADER.register(new ModuleHook<>(Utils.getResource("arrow_damage"), TargetDeathModifierHook.class, ArrowDamageModifierHook.AllMerger::new, (tool, modifier, context, arrow, baseDamage) -> {return (double) baseDamage}));
+            LOADER.register(new ModuleHook<>(Utils.getResource("arrow_damage"), ArrowDamageModifierHook.class, ArrowDamageModifierHook.AllMerger::new, (tool, modifier, context, arrow, baseDamage) -> baseDamage));
 }
