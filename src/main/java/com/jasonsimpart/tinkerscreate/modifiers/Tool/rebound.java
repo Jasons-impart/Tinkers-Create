8f3b5f0b9ea5adc22b12274728d52eace12d3d38
JasonQ1123 <nmuqindexiang@163.com>
2024-07-18 22:56:28 +0800

    死了

--- src/main/java/com/jasonsimpart/tinkerscreate/modifiers/Tool/rebound.java
+++ src/main/java/com/jasonsimpart/tinkerscreate/modifiers/Tool/rebound.java
@@ -1,7 +1,26 @@
 package com.jasonsimpart.tinkerscreate.modifiers.Tool;
 
+import com.jasonsimpart.tinkerscreate.hooks.ShieldBlockModifierHook;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.ShieldBlockEvent;
 import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.tools.helper.ToolAttackUtil;
+import slimeknights.tconstruct.library.tools.helper.ToolDamageUtil;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
-public class rebound extends Modifier {
+public class rebound extends Modifier implements ShieldBlockModifierHook {
 
+    @Override
+    public void onShieldBlocking(IToolStackView tool, ShieldBlockEvent event, Player player, DamageSource source, int level) {
+        Entity entity = event.getDamageSource().getDirectEntity();
+        if (entity instanceof LivingEntity target) {
+            if (player.getUseItemRemainingTicks() > 71960) {
+                ToolAttackUtil.extraEntityAttack(tool, player, player.getUsedItemHand(), target);
+                ToolDamageUtil.damage(tool, 5, player, player.getUseItem());
+            }
+        }
+    }
 }
